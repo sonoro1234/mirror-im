0371cef986059206979f22fd0dc03c707fa2f769
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2009-07-06 04:28:05 +0000

    *** empty log message ***

--- trunk/im/src/im_lib.cpp
+++ trunk/im/src/im_lib.cpp
@@ -2,7 +2,7 @@
  * \brief Library Management
  *
  * See Copyright Notice in im_lib.h
- * $Id: im_lib.cpp,v 1.3 2008-12-11 19:07:54 scuri Exp $
+ * $Id: im_lib.cpp,v 1.4 2009-07-06 04:28:05 scuri Exp $
  */
 
 #include <stdlib.h>
@@ -11,8 +11,8 @@
 #include "im_lib.h"
 
 /* This appears only here to avoid changing the iup.h header fo bug fixes */
-#define IM_VERSION_FIX ".1"
-#define IM_VERSION_FIX_NUMBER 1
+#define IM_VERSION_FIX ".2"
+#define IM_VERSION_FIX_NUMBER 2
 
 static char *iVersion = "TECVERID.str:IM:LIB:" IM_VERSION IM_VERSION_FIX;
 
