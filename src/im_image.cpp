8fbc995ed21de5cf6949553addba08a32aa65a04
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2010-01-17 18:18:12 +0000

    New: function  imImageSetAlpha.

--- src/im_image.cpp
+++ src/im_image.cpp
@@ -2,7 +2,7 @@
  * \brief Image Manipulation
  *
  * See Copyright Notice in im_lib.h
- * $Id: im_image.cpp,v 1.6 2010-01-15 17:21:47 scuri Exp $
+ * $Id: im_image.cpp,v 1.7 2010-01-17 18:18:12 scuri Exp $
  */
 
 #include <stdlib.h>
@@ -271,6 +271,39 @@ void imImageClear(imImage* image)
 
   if (image->has_alpha)
     memset(image->data[image->depth], 0, image->plane_size);
+}
+
+template <class T> 
+inline void iSet(T *map, T value, int count)
+{
+  for (int i = 0; i < count; i++)
+  {
+    *map++ = value;
+  }
+}
+  
+void imImageSetAlpha(imImage* image, float alpha)
+{
+  assert(image);
+
+  if (image->has_alpha)
+  {
+    switch(image->data_type)
+    {
+    case IM_BYTE:
+      memset(image->data[image->depth], (imbyte)alpha, image->plane_size);
+      break;                                                                                
+    case IM_USHORT:                                                                           
+      iSet((imushort*)image->data[image->depth], (imushort)alpha, image->plane_size);
+      break;                                                                                
+    case IM_INT:                                                                           
+      iSet((int*)image->data[image->depth], (int)alpha, image->plane_size);
+      break;                                                                                
+    case IM_FLOAT:                                                                           
+      iSet((float*)image->data[image->depth], (float)alpha, image->plane_size);
+      break;                                                                                
+    }
+  }
 }
 
 int imImageIsBitmap(const imImage* image)
