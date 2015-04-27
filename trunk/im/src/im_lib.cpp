1a36b2d89ae13d135ec340a0eaeaec45f0a03324
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2015-04-27 18:09:24 +0000

    preparing 3.9.1

--- trunk/im/src/im_lib.cpp
+++ trunk/im/src/im_lib.cpp
@@ -10,9 +10,9 @@
 #include "im_lib.h"
 
 /* This appears only here to avoid changing the im_lib.h header for bug fixes */
-#define IM_VERSION_FIX ""
-#define IM_VERSION_FIX_NUMBER 0
-/* #define IM_VERSION_FIX_DATE "2013/11/21" */
+#define IM_VERSION_FIX ".1"
+#define IM_VERSION_FIX_NUMBER 1
+#define IM_VERSION_FIX_DATE "2015/04/27"
 
 static const char *iVersion = "TECVERID.str:IM:LIB:" IM_VERSION IM_VERSION_FIX;
 
