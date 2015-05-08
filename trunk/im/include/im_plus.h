843659b8ef112a0768800ecd2fb26d402cdc7442
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2015-05-08 17:40:05 +0000

    updated imImageFile class

--- trunk/im/include/im_plus.h
+++ trunk/im/include/im_plus.h
@@ -7,7 +7,10 @@
 #ifndef __IM_PLUS_H
 #define __IM_PLUS_H
 
- 
+#include "im.h"
+#include "im_raw.h"
+
+
 /** \brief Image File Wrapper Class
  *
  * \par
@@ -17,6 +20,7 @@
  *  \ingroup file */
 class imImageFile
 {
+protected:
   imFile* ifile;
   int error;
 
@@ -30,7 +34,7 @@ public:
   imImageFile(const char* file_name, const char* format)
     { this->ifile = imFileNew(file_name, format, &this->error); }
 
-  ~imImageFile()
+  virtual ~imImageFile()
     { if (this->ifile) imFileClose(this->ifile); }
 
   int Failed() 
@@ -41,9 +45,27 @@ public:
 
   void SetAttribute(const char* attrib, int data_type, int count, const void* data)
     { imFileSetAttribute(this->ifile, attrib, data_type, count, data); }
+    
+  void SetAttribInteger(const char* attrib, int data_type, int value)
+    { imFileSetAttribInteger(this->ifile, attrib, data_type, value); }
+
+  void SetAttribReal(const char* attrib, int data_type, double value)
+    { imFileSetAttribReal(this->ifile, attrib, data_type, value); }
+
+  void SetAttribString(const char* attrib, const char* value)
+    { imFileSetAttribString(this->ifile, attrib, value); }
 
   const void* GetAttribute(const char* attrib, int *data_type, int *count)
     { return imFileGetAttribute(this->ifile, attrib, data_type, count); }
+    
+  const int GetAttribInteger(const char* attrib, int index)
+    { return imFileGetAttribInteger(this->ifile, attrib, index); }
+
+  const double GetAttribReal(const char* attrib, int index)
+    { return imFileGetAttribReal(this->ifile, attrib, index); }
+
+  const char* GetAttribString(const char* attrib)
+    { return imFileGetAttribString(this->ifile, attrib); }
 
   void GetInfo(char* format, char* compression, int *image_count)
     { imFileGetInfo(this->ifile, format, compression, image_count); }
@@ -69,5 +91,21 @@ public:
   void WriteImageData(void* data)
     { this->error = imFileWriteImageData(this->ifile, data); }
 };
+
+
+/** \brief Image File Raw Wrapper Class
+*
+*  \ingroup file */
+class imImageFileRaw : public imImageFile
+{
+  imImageFileRaw() {};
+
+public:
+
+  imImageFileRaw(const char* file_name, bool new_file): imImageFile()
+    { if (new_file) this->ifile = imFileNewRaw(file_name, &this->error);
+      else this->ifile = imFileOpenRaw(file_name, &this->error); }
+};
+
 
 #endif
