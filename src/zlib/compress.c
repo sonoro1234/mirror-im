c13aa829de9abd9fe98e9b2dbb9f162a95d60c79
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2015-12-18 21:40:20 +0000

    Changed: zlib updated to version 1.2.8

--- src/zlib/compress.c
+++ src/zlib/compress.c
@@ -3,7 +3,7 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* @(#) $Id: compress.c,v 1.2 2011-12-22 17:54:47 scuri Exp $ */
+/* @(#) $Id$ */
 
 #define ZLIB_INTERNAL
 #include "zlib.h"
@@ -29,7 +29,7 @@ int ZEXPORT compress2 (dest, destLen, source, sourceLen, level)
     z_stream stream;
     int err;
 
-    stream.next_in = (Bytef*)source;
+    stream.next_in = (z_const Bytef *)source;
     stream.avail_in = (uInt)sourceLen;
 #ifdef MAXSEG_64K
     /* Check for source > 64K on 16-bit machine: */
