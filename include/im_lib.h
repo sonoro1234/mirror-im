0ed387593f7c3c2e712a41a7271c8d35425876ab
scuri <scuri@af3c9e47-74de-4ca0-9433-524f997074d0>
2017-01-19 23:02:25 +0000

    copyright year updated to 2017

--- include/im_lib.h
+++ include/im_lib.h
@@ -31,7 +31,7 @@ extern "C" {
  */
 #define IM_NAME "IM - An Imaging Toolkit"
 #define IM_DESCRIPTION "Toolkit for Image Representation, Storage, Capture and Processing"
-#define IM_COPYRIGHT "Copyright (C) 1994-2016 Tecgraf/PUC-Rio"
+#define IM_COPYRIGHT "Copyright (C) 1994-2017 Tecgraf/PUC-Rio"
 #define IM_AUTHOR "Antonio Scuri"
 #define IM_VERSION "3.12"      /* bug fixes are reported only by imVersion functions */
 #define IM_VERSION_NUMBER 312000
